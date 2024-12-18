c88c823fabb8605c24884675a47c1ffc08407ebc
Kevin Sangani <96529851+Kevin0403@users.noreply.github.com>
2024-12-18 23:49:20 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import Home from "./Home"
 import About from "./About"
 import Contact from "./Contact"
-import Blog from "./Blog"
 import Work from "./Work"
 import ProjectPage from "./ProjectPage"
-import BlogPage from "./BlogPage"
 
 export {
-    BlogPage,
     ProjectPage,
     Home,
     About,
     Contact,
-    Blog,
     Work
 }
